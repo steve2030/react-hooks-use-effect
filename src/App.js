a90e614b10d396cc26c4e6e854265d39bbe61941
steve <stevemighty2030@gmail.com>
2022-09-27 06:26:43 +0300

    the assignment is complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text])
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  })
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return <div className="container">
+ <button onClick={(()=> setCount((prevState) => prevState + 1))} > I've been clicked  {count} times</button>
+ <br></br><br></br>
+  <input type="text"
+  placeholder="Type away...."
+  onChange={(e) => setText(e.target.value)}/>
+  <DogPics/>
+  </div>
+
 }
 
 export default App;
--- src/DogPics.js
+++ src/DogPics.js
@@ -25,5 +25,4 @@ function DogPics() {
     </div>
   );
 }
-
 export default DogPics;
